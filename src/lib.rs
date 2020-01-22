f62885726148d635668113d0b83a5a7f41d8843d
vitocchi <vitocchi.mm@gmail.com>
2020-01-22 23:41:51 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@ extern crate serde;
 extern crate hex;
 use eng_wasm::*;
 use eng_wasm_derive::pub_interface;
-pub mod accounts;
+mod account_service;
+use account_service::{AccountService, AccountRepositoryInterface};
+mod accounts;
 use accounts::{Id, Pass, Accounts};
 
 /*
@@ -16,11 +18,10 @@ pub static SECRET_ACCOUNTS: &str = "secret_accounts";
 // Public struct Contract which will consist of private and public-facing secret contract functions
 pub struct Contract;
 
+// Private functions accessible only by the secret contract
 impl Contract {
     fn service() -> AccountService<AccountRepository> {
-        return AccountService {
-            repository: AccountRepository{},
-        };
+        AccountService::new(AccountRepository{})
     }
 }
 
@@ -82,60 +83,5 @@ impl ContractInterface for Contract {
     #[no_mangle]
     fn get_account_ids() -> Vec<Id> {
         Self::service().get_account_ids()
-    }
-}
-
-trait AccountRepositoryInterface {
-    fn get_accounts(&self) -> Accounts;
-    fn store_accounts(&self, accounts: Accounts);
-}
-
-struct AccountService<T> {
-    repository: T,
-}
-
-impl <T: AccountRepositoryInterface> AccountService<T> {
-    fn registor(self, id: Id, pass: Pass) -> bool{
-        let mut accounts = self.repository.get_accounts();
-        let successed = match accounts.registor(id, pass) {
-            Ok(_) => {
-                self.repository.store_accounts(accounts);
-                true
-            },
-            Err(_) => {
-                false
-            },
-        };
-       return successed
-    }
-
-    /*
-    fn registor_without_pass(id: Id) -> Pass {
-        let mut accounts = Self::get_accounts();
-        match accounts.registor_without_pass(id) {
-            Ok(pass) => {
-                write_state!(SECRET_ACCOUNTS => accounts);
-                pass
-            },
-            Err(_) => {
-                [0;, 32]
-            }
-        }
-    }
-    */
-    
-    fn authorize(&self, id: Id, pass: Pass) -> bool {
-        let accounts = self.repository.get_accounts();
-        accounts.authorize(id, pass) == Ok(())
-    }
-
-    fn is_exist(&self, id: Id) -> bool {
-        let accounts = self.repository.get_accounts();
-        accounts.is_exist(id)
-    }
-
-    fn get_account_ids(&self) -> Vec<Id> {
-        let mut accounts = self.repository.get_accounts();
-        accounts.retrieve_all_account_ids()
     }
 }
