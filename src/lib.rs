df56536aa7047e45855776a0c0d9f2d5c1a51f39
vitocchi <vitocchi.mm@gmail.com>
2020-01-22 22:58:55 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,22 @@ pub static SECRET_ACCOUNTS: &str = "secret_accounts";
 // Public struct Contract which will consist of private and public-facing secret contract functions
 pub struct Contract;
 
-// Private functions accessible only by the secret contract
 impl Contract {
-    fn get_accounts() ->  Accounts {
+    fn service() -> AccountService<AccountRepository> {
+        return AccountService {
+            repository: AccountRepository{},
+        };
+    }
+}
+
+struct AccountRepository;
+
+impl AccountRepositoryInterface for AccountRepository{
+    fn get_accounts(&self) -> Accounts {
         read_state!(SECRET_ACCOUNTS).unwrap()
+    }
+    fn store_accounts(&self, accounts: Accounts) {
+        write_state!(SECRET_ACCOUNTS => accounts);
     }
 }
 
@@ -38,17 +50,7 @@ pub trait ContractInterface {
 impl ContractInterface for Contract {
     #[no_mangle]
     fn registor(id: Id, pass: Pass) -> bool{
-        let mut accounts = Self::get_accounts();
-        let successed = match accounts.registor(id, pass) {
-            Ok(_) => {
-                write_state!(SECRET_ACCOUNTS => accounts);
-                true
-            },
-            Err(_) => {
-                false
-            },
-        };
-       return successed
+        Self::service().registor(id, pass)
     }
 
     /*
@@ -68,19 +70,72 @@ impl ContractInterface for Contract {
     
     #[no_mangle]
     fn authorize(id: Id, pass: Pass) -> bool {
-        let accounts = Self::get_accounts();
-        accounts.authorize(id, pass) == Ok(())
+        Self::service().authorize(id, pass)
     }
+
 
     #[no_mangle]
     fn is_exist(id: Id) -> bool {
-        let accounts = Self::get_accounts();
-        accounts.is_exist(id)
+        Self::service().is_exist(id)
     }
 
     #[no_mangle]
     fn get_account_ids() -> Vec<Id> {
+        Self::service().get_account_ids()
+    }
+}
+
+trait AccountRepositoryInterface {
+    fn get_accounts(&self) -> Accounts;
+    fn store_accounts(&self, accounts: Accounts);
+}
+
+struct AccountService<T> {
+    repository: T,
+}
+
+impl <T: AccountRepositoryInterface> AccountService<T> {
+    fn registor(self, id: Id, pass: Pass) -> bool{
+        let mut accounts = self.repository.get_accounts();
+        let successed = match accounts.registor(id, pass) {
+            Ok(_) => {
+                self.repository.store_accounts(accounts);
+                true
+            },
+            Err(_) => {
+                false
+            },
+        };
+       return successed
+    }
+
+    /*
+    fn registor_without_pass(id: Id) -> Pass {
         let mut accounts = Self::get_accounts();
+        match accounts.registor_without_pass(id) {
+            Ok(pass) => {
+                write_state!(SECRET_ACCOUNTS => accounts);
+                pass
+            },
+            Err(_) => {
+                [0;, 32]
+            }
+        }
+    }
+    */
+    
+    fn authorize(&self, id: Id, pass: Pass) -> bool {
+        let accounts = self.repository.get_accounts();
+        accounts.authorize(id, pass) == Ok(())
+    }
+
+    fn is_exist(&self, id: Id) -> bool {
+        let accounts = self.repository.get_accounts();
+        accounts.is_exist(id)
+    }
+
+    fn get_account_ids(&self) -> Vec<Id> {
+        let mut accounts = self.repository.get_accounts();
         accounts.retrieve_all_account_ids()
     }
 }
